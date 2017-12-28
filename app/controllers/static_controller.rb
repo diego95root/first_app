dcca8c39aac19a378dc6cbb66184ef1f64b725a1
diego bernal adelantado <diego@MacBook-Air-2.local>
2017-12-28 01:36:41 +0100

    Added routes for pages, home and help

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,11 @@
+class StaticController < ApplicationController
+  def pages
+  end
+
+  def home
+  end
+
+  def help
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'static/pages'
+  get 'static/home'
+  get 'static/help'
+  root 'static#home'
+
   get 'welcome/index'
   resources :articles
   root 'welcome#index'
